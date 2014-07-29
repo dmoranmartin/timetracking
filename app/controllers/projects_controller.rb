3c2918cf78c6782b77ca015afd7f2b574c19f828
vagrant <vagrant@ironhack>
2014-07-29 14:55:50 +0000

    Fourth commit

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,6 +6,6 @@ class ProjectsController < ApplicationController
 	def show
 		@project = Project.find params[:id]
 		rescue ActiveRecord::RecordNotFound
-	  		render 'error'
+	  		render 'error', layout: "showlay"
 	end
 end
