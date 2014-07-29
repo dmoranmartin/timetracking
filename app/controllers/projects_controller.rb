10253a4b67dc610bf78d9e7e399437816917a900
vagrant <vagrant@ironhack>
2014-07-29 11:31:24 +0000

    Second commit

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,9 @@
+class ProjectsController < ApplicationController
+	def index
+		@projects = Project.last_created_projects(10)
+	end
+
+	def show
+		@project = Project.find params[:id]
+	end
+end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,14 @@
 class Project < ActiveRecord::Base
+	def self.iron_find(id)
+	 project = where(id: id).first
+		 unless project.present?
+		 	raise
+		 	ActiveRecordd::RecordNotFound.new("Couldn't find project with id=#{id}")
+		 end
+	 project
+	end
+
+	def self.last_created_projects(n)
+		Project.order(created_at: :desc).limit(n)
+	end
 end
